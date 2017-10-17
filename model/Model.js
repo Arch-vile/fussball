d3417b01e5647444a9a91b7c943492b63757d198
Mikko Ravimo <mrav@FL911.home>
2017-10-17 20:32:50 +0300

    Working algorithm for game creation

--- model/Model.js
+++ model/Model.js
@@ -37,38 +37,37 @@ class Tournament {
 
     generateMatches() {
         assert(this.players.length >= 4, 'Tournament needs atleast four players')
+        assert(this.gamesPerPlayer * this.players.length % 4 == 0, 'Number of games per player multiplied by number of players needs to divisable with four')
 
-        const matchesPerPlayer = 4;
-        console.log(`Generating matches for ${this.players.length} players with ${matchesPerPlayer} matches per player`)
+        console.log(`Generating games for ${this.players.length} players with ${this.gamesPerPlayer} games per player`)
         
         var randomOrderedNamePool = []
-        for(var i = 0; i < matchesPerPlayer; i++) {
-            randomOrderedNamePool = randomOrderedNamePool.concat(_.shuffle(this.players))
-        }
+        const games = []
+        // Until we have as many matches as we asked for
+        while(games.length < this.gamesPerPlayer * this.players.length / 4) {
 
-        var matches = []
-        for(var i = 0; i < randomOrderedNamePool.length; i+=4) {
-            matches.push(new Match(randomOrderedNamePool[i],randomOrderedNamePool[i+1],randomOrderedNamePool[i+2],randomOrderedNamePool[i+3]))
-        }
+            // If there is not enough players left for a game
+            while(randomOrderedNamePool.length < 4) {
+                const newNamePool = _.clone(_.shuffle(this.players))
 
-        // There is a small change for one player to end up twice in same match, and as I am lazy and stupid lets just brute force
-        if( this.anyDuplicatePlayers(matches)  ) {
-            return this.generateMatches();
-        } else {
-            return matches;
+                // We need to inspect the next four players as there is change that the remaining players of previous pool include the first players of the new pool
+                if(this.hasUniquePlayers(this.takeFirstFour(randomOrderedNamePool, newNamePool))) {
+                    randomOrderedNamePool = randomOrderedNamePool.concat(newNamePool) 
+                }
+
+            }
+            games.push(new Match(randomOrderedNamePool.shift(),randomOrderedNamePool.shift(),randomOrderedNamePool.shift(),randomOrderedNamePool.shift()))
         }
+    
+        return games;
     }
 
-    anyDuplicatePlayers(matches) {
-        for(var m = 0; m < matches.length; m++) {
-            const match = matches[m]
-            const players = [ match.team1Player1, match.team1Player2, match.team2Player1, match.team2Player2 ]
-            if(new Set(players).size != 4) {
-                console.log("SAME")
-                return true
-            } 
-        }
-        return false
+    takeFirstFour(array1, array2) {
+        return array1.concat(array2).slice(0,4)
+    }
+
+    hasUniquePlayers(players) {
+        return new Set(players).size == 4;
     }
 
 }
