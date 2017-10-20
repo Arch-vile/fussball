10a834507ef50b53f7a7532cffc74eeb7ae14626
Mikko Ravimo <mrav@FL911.home>
2017-10-21 00:38:24 +0300

    add start script to package.json

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+var expressMongoDb = require('express-mongo-db');
+
+const app = express()
+var bodyParser = require('body-parser');
+const model = require('./model/Model.js')
+const Repository = require('./model/Repository.js')
+
+app.use(expressMongoDb(process.env.mongoURI));
+app.use(express.static('public'))
+app.use(bodyParser())
+
+app.post('/tournaments', function(req,res){
+    new Repository(req.db).createTournament(req.body,function(err,data){
+        res.send({"id": data.insertedId})
+    })
+})
+
+app.get('/tournaments', function(req,res){
+    new Repository(req.db).getTournaments(function(err,data){
+        res.send({ 'tournaments': data })
+    })
+})
+
+app.get('/tournaments/:id', function(req,res){
+    new Repository(req.db).getTournament(req.params.id,function(err,data){
+        res.send(data)
+    })
+})
+
+app.put('/tournaments/:tId/games/:gId', function(req,res){
+    new Repository(req.db).updateGame(req.params.tId, req.body, function(err,data){
+        if(err)
+            res.send(500)
+        else
+            res.send(data)
+    })
+})
+
+ // views for UI
+app.get('/tournaments/:id/views/scoreboard', function(req,res){
+    new Repository(req.db).leaderBoard(req.params.id, function(err,leaderBoard) {
+        res.send({ 'scores': leaderBoard })
+    })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
